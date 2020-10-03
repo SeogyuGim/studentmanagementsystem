b780083b05e408f3912cb104b8a35a2a6a10d4b4
Yeriel Kim <asap0208@gmail.com>
2020-10-03 16:18:31 +0900

    add lectures, waitlist

--- backend/apps/certificates/admin.py
+++ backend/apps/certificates/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Certificate
 
-# Register your models here.
+
+class CertificateAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_datetime', 'updated_datetime',)
+    search_fields = ('name',)
+
+
+admin.site.register(Certificate, CertificateAdmin)
--- backend/apps/certificates/views.py
+++ backend/apps/certificates/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import serializers, viewsets
+from .models import Certificate
+from rest_framework.response import Response
 
-# Create your views here.
+
+class CertificateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Certificate
+        fields = (
+            'id',
+            'name',
+            'description',
+            'created_datetime',
+            'updated_datetime',
+        )
+
+
+class CertificateViewSet(viewsets.ModelViewSet):
+    queryset = Certificate.objects.all()
+    serializer_class = CertificateSerializer
+
+    def get_paginated_response(self, data):
+        return Response(data)
