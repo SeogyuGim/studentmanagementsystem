b94c87eb099d9af3d754f77616c8cac9e809e76c
Yeriel Kim <asap0208@gmail.com>
2020-10-03 17:03:10 +0900

    finish

--- backend/apps/lectures/views.py
+++ backend/apps/lectures/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import serializers, viewsets
+from .models import Lecture
+from rest_framework.response import Response
 
-# Create your views here.
+
+class LectureSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lecture
+        fields = (
+            'id',
+            'title',
+            'description',
+            'lecturer_name',
+            'created_datetime',
+            'duration',
+            'slides_url',
+            'is_required',
+        )
+
+
+class LectureViewSet(viewsets.ModelViewSet):
+    queryset = Lecture.objects.all()
+    serializer_class = LectureSerializer
+
+    def get_paginated_response(self, data):
+        return Response(data)
